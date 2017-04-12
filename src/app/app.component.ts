c4460d86eeb06bf69b6b1cec7bd2513c65da0191
Satish Kumar Kamavaram <satishkamavaram@gmail.com>
2017-04-12 22:25:54 +0530

    observables added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
   import { Component,Output,OnInit } from '@angular/core';
 import {UserServices} from './services/User.services';
 import {CounterService} from './services/counter.service';
+import {Observable}  from 'rxjs/Observable';
+import 'rxjs/Rx';
+import {Observer} from 'rxjs/Observer';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -31,6 +34,40 @@ ngOnInit() {
     this.userServices.inactiveUsers.subscribe(
       (inactive:number) => (this.inactiveCount=inactive)
     );
+
+    //Observables
+    const testObservables  = Observable.create((observer : Observer<string>)=>
+  {
+      setTimeout(()=>{
+        observer.next('first observer');
+      },2000);
+      setTimeout(()=>{
+        observer.next('second observer');
+      },4000);
+      setTimeout(()=>{
+       observer.error('error observer');
+      },6000);
+      setTimeout(()=>{
+       observer.complete();
+     },8000);
+     setTimeout(()=>{
+       observer.next('third observer');
+     },10000);
+  });
+
+  testObservables.subscribe(
+    (data:String) =>{
+      console.log(data);
+    },
+    (error:String) =>{
+      console.log(error);
+    },
+    (complete:String) =>{
+      console.log(complete+" ,completed");
+    }
+
+  );
+
 }
 
 
